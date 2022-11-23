7d7e31e767a96bacfb5b58cdbec740133a4a0d6e
rssabbir <sabbirhossainrahad@gmail.com>
2022-11-23 23:44:48 +0600

    setup authcontext and set all operation for firebase

---
+++ src/context/AuthProvider/AuthProvider.js
@@ -0,0 +1,60 @@
+import {
+	createUserWithEmailAndPassword,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+	signInWithPopup,
+	updateProfile,
+} from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import auth from '../../firebase/firebase.config';
+
+export const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+	const [user, setUser] = useState(null);
+	const [userLoading, setUserLoading] = useState(false);
+
+	const createUser = (email, password) => {
+		setUserLoading(true);
+		return createUserWithEmailAndPassword(auth, email, password);
+	};
+	const updateUserProfileOnFirebase = (profileData) => {
+		setUserLoading(true);
+		return updateProfile(auth.currentUser, profileData);
+	};
+
+	const loginUser = (email, password) => {
+		setUserLoading(true);
+		return signInWithEmailAndPassword(auth, email, password);
+	};
+	const providerLogin = (provider) => {
+		setUserLoading(true);
+		return signInWithPopup(auth, provider);
+	};
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+			setUserLoading(false);
+		});
+		return () => {
+			unsubscribe();
+		};
+	}, []);
+	const authInfo = {
+		user,
+		userLoading,
+		createUser,
+		updateUserProfileOnFirebase,
+		loginUser,
+		providerLogin,
+	};
+	return (
+		<>
+			<AuthContext.Provider value={authInfo}>
+				{children}
+			</AuthContext.Provider>
+		</>
+	);
+};
+
+export default AuthProvider;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AuthProvider from './context/AuthProvider/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = QueryClient();
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</QueryClientProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
