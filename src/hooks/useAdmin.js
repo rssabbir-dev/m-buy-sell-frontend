ccd075ebb6e47577519a9c616a124582e8beab7f
rssabbir <sabbirhossainrahad@gmail.com>
2022-11-23 23:46:01 +0600

    added useAdmin Hook

---
+++ src/hooks/useAdmin.js
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import auth from '../firebase/firebase.config';
+
+const useAdmin = () => {
+	const { data: isAdmin = false, isLoading: isAdminLoading } = useQuery({
+		queryKey: ['isAdmin'],
+		queryFn: async () => {
+			const res = await fetch(
+				`${process.env.REACT_APP_SERVER_URL}/user/admin/${auth.currentUser.uid}`
+			);
+			const data = await res.json();
+			return data.isAdmin;
+		},
+	});
+	return [isAdmin, isAdminLoading];
+};
+
+export default useAdmin;
